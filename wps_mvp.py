f9e5def1e143e7cef843ff9898832a5da39df428
Harlene Luy Fook <luyfookharlene@gmail.com>
2026-03-23 22:12:14 +0800

    Update wps_mvp.py

--- wps_mvp.py
+++ wps_mvp.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyMV3WBfVpx4YxpOovBdtTiC",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
